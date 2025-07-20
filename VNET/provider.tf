c04116d0b16de6727e88c2ec6b8a79649af8678d
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2025-07-20 17:03:51 +0100

    azure infra

---
+++ VNET/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.37.0"
+      }
+    }
+    
+}
+
+provider "azurerm" {
+  #configuration options
+  subscription_id = "7462b2b8-b655-488e-a6b0-2febf3ecc3c1"
+  features {}
+}
